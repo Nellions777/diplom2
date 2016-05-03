091435dc2985ef44fa1c3f1b816ec2ac6642ea62
Nesterenko Eygeniy <newjeka.777@mail.ru>
2016-05-03 11:59:32 +0300

    add simple param

--- src/main/webapp/js/fillParams.js
+++ src/main/webapp/js/fillParams.js
@@ -8,6 +8,7 @@ $(function() {
         paramsIdForDivOnPackages:[]
     };
     var parameters = [];
+    var divisors = [];
 
     $.ajax({
         type: "GET",
@@ -22,7 +23,8 @@ $(function() {
                     value:0.0,
                     start:0.0,
                     finish:0.0,
-                    step:0.0
+                    step:0.0,
+                    isDivisor:false
                 });
             }
             generationParams();
@@ -30,49 +32,70 @@ $(function() {
     });
 
     generationParams = function(){
+        var sumCounts = 1;
         var html = "";
         html += "<table>";
-        html += "<thead><tr><th>Fixed</th><th>Param value</th></tr></thead>";
+        html += "<thead><tr><th>Fixed</th><th>isDivisor</th><th>Param value</th></tr></thead>";
         html += "<tbody>";
         for(var i=0; i<parameters.length; i++){
             html += "<tr>";
             if(parameters[i].step === 0){
 
                 html += "<td><input type='checkbox' checked onchange='changeIsFixed(" + i + ",false)' /></td>";
+                html += "<td><input type='checkbox' disabled /></td>"
                 html += '<td><label>' + parameters[i].param.name + ": ";
                 html += "<input id='" + parameters[i].param.id + "' type='text' value='";
                 html += parameters[i].value;
                 html += "'/></td>";
-
             }else{
                 html += "<td><input type='checkbox' onchange='changeIsFixed(" + i + ",true)' /></td>";
-                html += '<td><label>' + parameters[i].param.name + ": ";
-                html += "<label>start: <input id='" + parameters[i].param.id + "_START' type='text' value='" + parameters[i].start + "' /></label>&nbsp&nbsp";
-                html += "<label>finish: <input id='" + parameters[i].param.id + "_FINISH' type='text' value='" + parameters[i].finish + "' /></label>&nbsp&nbsp";
-                html += "<label>step: <input id='" + parameters[i].param.id + "_STEP' type='text' value='" + parameters[i].step + "' /></label></td>";
+                html += "<td><input type='checkbox' ";
+                    if(parameters[i].isDivisor) {
+                        html += "checked ";
+                        sumCounts *= divisors[i];
+                    }
+                html += "onchange='saveDivisor("+ i +")' /></td>";
+                html += '<td>' + parameters[i].param.name + ": ";
+                html += "<label>start: <input id='" + parameters[i].param.id + "_START' type='text' value='" + parameters[i].start + "' onblur='updateParameters("+i+",-1);generationParams()' /></label>&nbsp&nbsp";
+                html += "<label>finish: <input id='" + parameters[i].param.id + "_FINISH' type='text' value='" + parameters[i].finish + "' onblur='updateParameters("+i+",-1);generationParams()' /></label>&nbsp&nbsp";
+                html += "<label>step: <input id='" + parameters[i].param.id + "_STEP' type='text' value='" + parameters[i].step + "' onblur='updateParameters("+i+",-1);generationParams()' /></label>&nbsp&nbsp";
+                html += "count: <span id='" + parameters[i].param.id + "_COUNT'>" + divisors[i] + "</span></td>";
             }
             html += "<tr>";
         }
+        html += "</tbody>";
+        html += "</table>";
+        html += "count of packages: <span>" + sumCounts + "</span></td>";
         $('#params').html(html);
+    };
+
+    saveDivisor = function(index){
+        if(parameters[index].isDivisor === false){
+            parameters[index].isDivisor = true;
+        }
+        else parameters[index].isDivisor = false;
+        divisors[index] = Math.ceil(((parameters[index].finish - parameters[index].start)/parameters[index].step)+1);
+        generationParams();
     };
 
     changeIsFixed = function(index,isFixed){
         if(isFixed) {
             parameters[index].step = 0;
             parameters[index].value = 0;
+            parameters[index].isDivisor = false;
         }
         else {
             parameters[index].step = 1;
             parameters[index].start = 0;
             parameters[index].finish = 0;
         }
-        updateParameters(index);
+        updateParameters(index,index);
         generationParams();
     };
 
-    updateParameters = function(index) {
+    updateParameters = function(index, executing) {
         for(var i=0; i<parameters.length; i++){
-            if(i != index){
+            if(i != executing){
                 if(parameters[i].step == 0){
                     parameters[i].value = $('#' + parameters[i].param.id).val();
                 }else{
@@ -82,11 +105,12 @@ $(function() {
                 }
             }
         }
-    }
-    //params post to server
+        if(index != -1) divisors[index] = Math.ceil(((parameters[index].finish - parameters[index].start)/parameters[index].step)+1);
+    };
+
     postParams = function(){
 
-        updateParameters(-1);
+        updateParameters(-1,-1);
 
         for(var i=0; i<parameters.length; i++){
             if(parameters[i].step == 0){
@@ -101,12 +125,10 @@ $(function() {
                     finish: parameters[i].finish,
                     step: parameters[i].step
                 });
+                if(parameters[i].isDivisor) parameterForServer.paramsIdForDivOnPackages.push(parameters[i].param.id);
             }
         }
-        parameterForServer.paramsIdForDivOnPackages.push(1);
-        parameterForServer.paramsIdForDivOnPackages.push(2);
 
-        //try send
         $.ajax({
             type: "POST",
             url: "/calculate",
@@ -118,5 +140,9 @@ $(function() {
                 html += "Обработка параметров закончена.</br>";
                 $('#outputResults').html(html);
             }});
+
+        parameterForServer.simpleParams.length = 0;
+        parameterForServer.rangeParams.length = 0;
+        parameterForServer.paramsIdForDivOnPackages.length = 0;
     }
 });
